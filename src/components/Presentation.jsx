bfc844dc8c89193ce61883688e249762c805df8f
andredomin <andres10dominguez@gmail.com>
2025-02-03 21:42:30 +0100

    optimize

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -6,7 +6,7 @@ const Presentation = () => {
     <>
         <header>
         <Starfield
-                starCount={1000}
+                starCount={100}
                 starColor={[255, 255, 255]}
                 speedFactor={0.05}
                 backgroundColor="black"
